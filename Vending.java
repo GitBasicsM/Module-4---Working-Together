b96e927db97573bac6da763cb8454579e4ebb44a
Zbroadbent2 <zachbroadbent04@gmail.com>
2024-02-16 14:14:23 -0500

    static class snack added

--- Vending.java
+++ Vending.java
@@ -61,5 +61,21 @@ public class Vending {
   
           System.out.println("Thank you for using the Vending Machine. Have a great day!");
       }
-  
+      static class Snack {
+        private final String name;
+        private final double price;
+
+        public Snack(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
     }
+}
